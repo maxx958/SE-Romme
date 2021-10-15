bc5ad9fcb3a410b8894e94fcd21c7398f862b18b
maxx958 <maxim.binder@web.de>
2021-10-15 12:59:14 +0200

    d

--- rommé/src/test/scala/Test1.scala
+++ rommé/src/test/scala/Test1.scala
@@ -1,6 +1,18 @@
-import org.junit.Test
-import org.junit.Assert.*
+package Rommé
 
-class Test1:
-  @Test def t1(): Unit = 
-    assertEquals("I was compiled by Scala 3. :)", msg)
+import scala.io.StdIn._
+
+object test {
+  def main(args: Array[String]) = {
+    println("Welcome to RommÃ© \n")
+    val greeting = "Hello " + signUp(args)
+    println(greeting)
+  }
+
+  def signUp(playerNames: Array[String]): String = {
+    if (playerNames.length > 0)
+      playerNames.head
+    else
+      readLine("Please enter your name: ")
+  }
+}
